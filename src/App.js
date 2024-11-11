dc216f0897c5e99d940e4d81552b1e6c8f2b6bbd
pulemashile <166323606+pulemashile@users.noreply.github.com>
2024-11-11 10:45:35 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Footer from "./components/footer-component/Footer.js";
+import Hero from "./components/hero-component/Hero.js";
+import Navbar from "./components/navbar-component/Navbar.js";
+
+
+function App() {
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+      <Footer />
+    </main>
+  );
+}
+
+export default App;
